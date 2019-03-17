ea7f39c3b9f781ce66a8b8400d81ae31d19851cc
unknown <ilyrodriguez@gmail.com>
2019-03-17 17:43:02 -0400

    hide function working so far

---
+++ assets/javascript/game.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+
+    var character = [{
+        name: "Neo",
+        healthPoints: 120,
+        attackPower: 150,
+        counterAttachPower: 10,
+    },
+    {
+        name: "Trinity",
+        healthPoints: 50,
+        attackPower: 100,
+        counterAttachPower: 20,
+    },
+    {
+        name: "Morpheus",
+        healthPoints: 100,
+        attackPower: 110,
+        counterAttachPower: 5,
+    },
+    {
+        name: "Smith",
+        healthPoints: 150,
+        attackPower: 100,
+        counterAttachPower: 15,
+    },
+    {
+        name: "Twins",
+        healthPoints: 120,
+        attackPower: 120,
+        counterAttachPower: 5,
+    },
+    {
+        name: "Sentinel",
+        healthPoints: 200,
+        attackPower: 110,
+        counterAttachPower: 10,
+    }]
+
+var heroeHealth = "";
+var heroeAttack = "";
+var heroeCounter = "";
+
+$("#neoB,#trinityB,#morpheusB,#smithB,#twinsB,#sentinelB").hide();
+
+
+});
